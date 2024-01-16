86d6a06491a381d8014f99f59e3e1daa96abaf9b
l.Daniel <losonczidani2001@gmail.com>
2024-01-16 21:33:25 +0100

    working local planner and control

--- globalPathPlan/src/CarlaGlobalPathPlanner/src/classes/frenet_planner.py
+++ globalPathPlan/src/CarlaGlobalPathPlanner/src/classes/frenet_planner.py
@@ -38,6 +38,13 @@ from cubic_spline_planner import CubicSpline2D
 import cubic_spline_planner
 
 SIM_LOOP = 500
+#Stanley Parameter
+k = 0.5  # control gain
+Kp = 1.0  # speed proportional gain
+dt = 0.1  # [s] time difference
+L = 2.79  # [m] Wheel base of vehicle
+max_steer = np.radians(30.0)  # [rad] max steering angle
+
 
 # Parameter
 MAX_SPEED = 50.0 / 3.6  # maximum speed [m/s]
@@ -103,6 +110,172 @@ class QuarticPolynomial:
 
         return xt
 
+class State(object):
+    """
+    Class representing the state of a vehicle.
+
+    :param x: (float) x-coordinate
+    :param y: (float) y-coordinate
+    :param yaw: (float) yaw angle
+    :param v: (float) speed
+    """
+
+    def __init__(self, x=0.0, y=0.0, yaw=0.0, v=0.0):
+        """Instantiate the object."""
+        super(State, self).__init__()
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        self.v = v
+        self.ax=None
+        self.ay=None
+        self.control_enabled=False
+        self.waypoints=None
+
+    def update(self, acceleration, delta):
+        """
+        Update the state of the vehicle.
+
+        Stanley Control uses bicycle model.
+
+        :param acceleration: (float) Acceleration
+        :param delta: (float) Steering
+        """
+        delta = np.clip(delta, -max_steer, max_steer)
+
+        self.x += self.v * np.cos(self.yaw) * dt
+        self.y += self.v * np.sin(self.yaw) * dt
+        self.yaw += self.v / L * np.tan(delta) * dt
+        self.yaw = normalize_angle(self.yaw)
+        self.v += acceleration * dt
+
+    def getLocalpath(self,msg):
+        self.waypoints = msg
+        waypoints = self.extract_waypoints()
+        self.ax, self.ay, wz =zip(*waypoints)
+        rospy.logwarn(len(self.ax))
+        self.control_enabled=True
+        
+    def extract_waypoints(self):
+            if self.waypoints is None:
+                rospy.logwarn("No global path received. Cannot extract waypoints.")
+                return None
+
+            waypoints = []
+            for pose_stamped in self.waypoints.poses:
+                waypoint = pose_stamped.pose
+                x = waypoint.position.x
+                y = waypoint.position.y
+                z = waypoint.position.z
+                rospy.logwarn('pozicio érkezik')
+                waypoints.append((x, y, z))  # Adatok hozzáadása a waypoints listához
+            rospy.logwarn("Subscribed {} waypoints.".format(len(waypoints)))
+            return waypoints
+
+def pid_control(target, current):
+    """
+    Proportional control for the speed.
+
+    :param target: (float)
+    :param current: (float)
+    :return: (float)
+    """
+    return Kp * (target - current)
+
+
+def stanley_control(state, cx, cy, cyaw, last_target_idx):
+    """
+    Stanley steering control.
+
+    :param state: (State object)
+    :param cx: ([float])
+    :param cy: ([float])
+    :param cyaw: ([float])
+    :param last_target_idx: (int)
+    :return: (float, int)
+    """
+    current_target_idx, error_front_axle = calc_target_index(state, cx, cy)
+
+    if last_target_idx >= current_target_idx:
+        current_target_idx = last_target_idx
+
+    # theta_e corrects the heading error
+    theta_e = normalize_angle(cyaw[current_target_idx] - state.yaw)
+    # theta_d corrects the cross track error
+    theta_d = np.arctan2(k * error_front_axle, state.v)
+    # Steering control
+    delta = theta_e + theta_d
+
+    return delta, current_target_idx
+
+
+def normalize_angle(angle):
+    """
+    Normalize an angle to [-pi, pi].
+
+    :param angle: (float)
+    :return: (float) Angle in radian in [-pi, pi]
+    """
+    while angle > np.pi:
+        angle -= 2.0 * np.pi
+
+    while angle < -np.pi:
+        angle += 2.0 * np.pi
+
+    return angle
+
+
+def calc_target_index(state, cx, cy):
+    """
+    Compute index in the trajectory list of the target.
+
+    :param state: (State object)
+    :param cx: [float]
+    :param cy: [float]
+    :return: (int, float)
+    """
+    # Calc front axle position
+    fx = state.x + L * np.cos(state.yaw)
+    fy = state.y + L * np.sin(state.yaw)
+
+    # Search nearest point index
+    dx = [fx - icx for icx in cx]
+    dy = [fy - icy for icy in cy]
+    d = np.hypot(dx, dy)
+    target_idx = np.argmin(d)
+
+    # Project RMS error onto front axle vector
+    front_axle_vec = [-np.cos(state.yaw + np.pi / 2),
+                      -np.sin(state.yaw + np.pi / 2)]
+    error_front_axle = np.dot([dx[target_idx], dy[target_idx]], front_axle_vec)
+
+    return target_idx, error_front_axle
+
+def calc_target_index(state, cx, cy):
+    """
+    Compute index in the trajectory list of the target.
+
+    :param state: (State object)
+    :param cx: [float]
+    :param cy: [float]
+    :return: (int, float)
+    """
+    # Calc front axle position
+    fx = state.x + L * np.cos(state.yaw)
+    fy = state.y + L * np.sin(state.yaw)
+
+    # Search nearest point index
+    dx = [fx - icx for icx in cx]
+    dy = [fy - icy for icy in cy]
+    d = np.hypot(dx, dy)
+    target_idx = np.argmin(d)
+
+    # Project RMS error onto front axle vector
+    front_axle_vec = [-np.cos(state.yaw + np.pi / 2),
+                      -np.sin(state.yaw + np.pi / 2)]
+    error_front_axle = np.dot([dx[target_idx], dy[target_idx]], front_axle_vec)
+
+    return target_idx, error_front_axle    
 
 class FrenetPath:
 
@@ -129,7 +302,7 @@ class FrenetOptimalPlannerNode:
     def __init__(self):
         rospy.init_node('frenet_optimal_planner_node', anonymous=True)
         self.global_path_sub = rospy.Subscriber('/global_path', Path, self.global_path_callback)
-        self.global_path_sub = rospy.Subscriber('/actual_pose', PoseStamped, self.get_actualPose_callback)
+        #self.global_path_sub = rospy.Subscriber('/actual_pose', PoseStamped, self.get_actualPose_callback)
 
         self.local_path_pub = rospy.Publisher('/local_path', Path, queue_size=1)
         self.global_path = None
@@ -143,12 +316,12 @@ class FrenetOptimalPlannerNode:
         self.robotpos_y=None
         self.robot_theta=None
     
-    def find_closest_point(self, path,):
+    def find_closest_point(self, path,vehicle_state):
         min_distance = float('inf')
         closest_index = None
 
         for i in range(len(path.x)):
-            distance = np.sqrt(np.power((path.x[i] - self.robotpos_x),2) + np.power((path.y[i] - self.robotpos_y),2))
+            distance = np.sqrt(np.power((path.x[i] - vehicle_state.x),2) + np.power((path.y[i] - vehicle_state.y),2))
             if distance < min_distance:
                 min_distance = distance
                 closest_index = i
@@ -210,9 +383,11 @@ class FrenetOptimalPlannerNode:
 
     
     def run(self):
-        rate = rospy.Rate(10)  # 10 Hz
+        br = tf.TransformBroadcaster()
+        rate = rospy.Rate(100)  # 100 Hz
          # initial state
-        c_speed = 10.0 / 3.6  # current speed [m/s]
+        target_speed = 30.0 / 3.6  # [m/s]
+        c_speed = target_speed  # current speed [m/s]
         c_accel = 0.0  # current acceleration [m/ss]
         c_d = 0.0  # current lateral position [m]
         c_d_d = 0.0  # current lateral speed [m/s]
@@ -226,98 +401,51 @@ class FrenetOptimalPlannerNode:
                     ])
         firstplan=False
         
+        state = State(x=-3.421, y=-8.410, yaw=np.radians(100.0), v=0.0)
+        while(self.frenet_planning_enabled==False):
+            pass
+        path = frenet_optimal_planning(self.csp, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd, ob)
         while not rospy.is_shutdown():
-            if self.frenet_planning_enabled: 
-                if firstplan==False:
-                    path = frenet_optimal_planning(
-                    self.csp, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd, ob)
-                    firstplan=True
-                while firstplan:
-                    current_time = rospy.Time.now()
-                    #rospy.logwarn("Frenet plan start")
-                
-                    closest_index, min_distance=self.find_closest_point(path)
-                    rospy.logwarn(min_distance)
-                    rospy.logwarn(closest_index)
-                    s0=path.s[closest_index]   
-                    c_d=path.d[closest_index]
-                    completion_percentage = closest_index / len(path.x) * 100.0
-                    if completion_percentage >= 90.0:
-                    # Trigger the generation of a new local path
-                        s0 = path.s[closest_index]  # Set the starting s value for the new path
-                        new_path = frenet_optimal_planning(self.csp, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd, ob)
+            #rospy.logwarn("Frenet plan start")
+          
+            
 
-                    # Update the existing path with the new path
-                        path = new_path             
-                    
-                    
-                    rospy.logwarn("local path")
-                    rospy.logwarn(len(path.x))
-                    # Convert best_path to ROS Path message
-                    local_path_msg_ros = Path()
-                    local_path_msg_ros.header.frame_id = "map"
-                    localpath=0
-                    if len(path.x):
-                        #self.publish_waypoints(path)
-                        for i in range(len(path.x)):
-                                                pose = PoseStamped()
-                                                pose.pose.position.x = path.x[i]
-                                                pose.pose.position.y = path.y[i]
-                                                local_path_msg_ros.poses.append(pose)
+            target_idx, _ = calc_target_index(state, path.x, path.y)
+            ai = pid_control(target_speed, state.v)
+            di, target_idx = stanley_control(state, path.x, path.y, path.yaw, target_idx)
+            state.update(ai, di)
 
-                        # Publish local path
-                        self.local_path_pub.publish(local_path_msg_ros)
-                    else:
-                        self.frenet_planning_enabled=False
-                        s0=0
-                    '''
-                    if len(path.x) > 1:
-                        # Publish the transform from /map to /base_link
-                        #for i in range(len(path.x) - 1):
-                        dx = path.x[1] - path.x[0]
-                        dy = path.y[1] - path.y[0]
-                        yaw = math.atan2(dy, dx)
-                        
-                        br.sendTransform((path.x[0], path.y[0], 0),
-                                        tf.transformations.quaternion_from_euler(0, 0,yaw),
-                                        rospy.Time.now(),
-                                        "base_link",
-                                        "map")     
-                    '''
-                    show_animation=False
-                    if show_animation:  # pragma: no cover
-                        rospy.logwarn("local path")
-
-                        # Convert best_path to ROS Path message
-                        local_path_msg_ros = Path()
-                        local_path_msg_ros.header.frame_id = "map"
-                        for i in range(len(path.x)):
+            
+            completion_percentage = closest_index / len(path.x) * 100.0
+            rospy.logwarn(min_distance, closest_index, completion_percentage)
+            if completion_percentage >= 90.0:
+                closest_index, min_distance=self.find_closest_point(path, state)
+                s0 = path.s[closest_index]   
+                c_d = min_distance
+                path = frenet_optimal_planning(self.csp, s0, c_speed, c_accel, c_d, c_d_d, c_d_dd, ob)         
+            
+            rospy.logwarn("local path")
+            rospy.logwarn(len(path.x))
+            # Convert best_path to ROS Path message
+            local_path_msg_ros = Path()
+            local_path_msg_ros.header.frame_id = "map"
+            if len(path.x):
+                #self.publish_waypoints(path)
+                for i in range(len(path.x)):
                                         pose = PoseStamped()
                                         pose.pose.position.x = path.x[i]
                                         pose.pose.position.y = path.y[i]
                                         local_path_msg_ros.poses.append(pose)
 
-                                    # Publish local path
-                        rospy.logwarn(local_path_msg_ros)
+                # Publish local path
+                self.local_path_pub.publish(local_path_msg_ros)
+                br.sendTransform((state.x, state.y, 0),
+                                tf.transformations.quaternion_from_euler(0, 0,state.yaw),
+                                rospy.Time.now(),
+                                "base_link",
+                                "map")     
+            
 
-                        self.local_path_pub.publish(local_path_msg_ros)
-                        plt.cla()
-                        # for stopping simulation with the esc key.
-                        #plt.gcf().canvas.mpl_connect(
-                        #'key_release_event',
-                        # lambda event: [exit(0) if event.key == 'escape' else None])
-                        plt.plot(self.tx, self.ty)
-
-                        plt.plot(ob[:, 0], ob[:, 1], "xk")
-                        plt.plot(path.x[1:], path.y[1:], "-or")
-                        plt.plot(path.x[1], path.y[1], "vc")
-                        plt.title("v[km/h]:" + str(c_speed * 3.6)[0:4])
-                        plt.grid(True)
-                        plt.show()
-                        
-
-                    print("Finish")
-    
 
                     
                     
